217dc880ad4355efa5d75887916f5978b74fd26b
arsiasieg <cam.lucas89@gmail.com>
2022-03-18 16:41:23 +0100

    Begin navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  icons : string[]
 
-  constructor() { }
+  constructor() {
+    this.icons = [
+      'home',
+      'person',
+      'school',
+      'work',
+      'email'
+    ]
+   }
 
   ngOnInit(): void {
   }
